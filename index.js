dcad17c4a02dbb777d96b8376f596cc1e112db73
Matthew <turbodieseldodge@gmail.com>
2023-01-07 16:30:40 -0600

    retake review

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2
+let num2 = 31
+let num3 = 1
+let num4 = 3
 
-// Write your code here
+const multiply = (num1 * num2)
+
+const random = Math.ceil(Math.random())
+
+const mod = (num3 + num4)
+
+const max = Math.max(5, 10, 15, 20)
